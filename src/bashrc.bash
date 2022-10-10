6d61a04e5fd838eae3806a6ac4e84a2d186e8ddd
Marc Groß <marc@marc-gross.de>
2022-10-10 10:26:51 +0200

    Fix an issue with ssh-agent when noclobber is set

--- src/bashrc.bash
+++ src/bashrc.bash
@@ -227,7 +227,7 @@ SSH_ENV="$HOME/.ssh/env"
 
 function start_agent {
      echo "Initialising new SSH agent..."
-     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+     /usr/bin/ssh-agent | sed 's/^echo/#echo/' >| "${SSH_ENV}"
      echo succeeded
      chmod 600 "${SSH_ENV}"
      . "${SSH_ENV}" > /dev/null
